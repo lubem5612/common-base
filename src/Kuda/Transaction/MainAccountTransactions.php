bf2c0c7ad89feb1e4dc0da067d212edd3523b9b6
admin <enginlubem@ymail.com>
2023-03-28 00:27:25 +0100

    facade development

---
+++ src/Kuda/Transaction/MainAccountTransactions.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Raadaapartners\Raadaabase\Kuda\Transaction;
+
+
+use Raadaapartners\Raadaabase\Helpers\ManageResponse;
+use Raadaapartners\Raadaabase\Kuda\Helpers\PostRequestHelper;
+
+class MainAccountTransactions
+{
+    use ManageResponse, PostRequestHelper;
+
+    private $page_size;
+    private $page_number;
+    private $start_date;
+    private $end_date;
+    private $serviceType;
+
+    public function __construct($PageSize, $PageNumber, $startDate = "", $endDate = "")
+    {
+        $this->page_size = $PageSize;
+        $this->page_number = $PageNumber;
+        $this->start_date = $startDate;
+        $this->end_date = $endDate;
+    }
+
+    public function handle()
+    {
+        try {
+            return $this->adminFetchAllTransactions();
+        }catch (\Exception $e) {
+            return $this->serverErrorResponse($e);
+        }
+    }
+
+    private function adminFetchAllTransactions()
+    {
+        $data = [
+            "pageSize" => $this->page_size,
+            "pageNumber" => $this->page_number,
+        ];
+        if (isset($this->start_date) && isset($this->end_date))
+        {
+            $data["startDate"] = $this->start_date;
+            $data["endDate"] = $this->end_date;
+            $this->serviceType = config('constants.transaction.main_account_filter');
+        }else {
+            $this->serviceType = config('constants.transaction.main_account');
+        }
+
+        $callback = $this->processKuda($this->serviceType, $data);
+        if ($callback["errors"]) {
+            return $this->errorResponse('error in fetching transactions', $callback['errors']);
+        }
+        return $this->successResponse('transactions list retrieved', $callback['data']['data']);
+    }
+}
