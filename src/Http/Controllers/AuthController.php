cf3ab288cd4f8ca5cc375f00c5c185718075c77e
Lubem Tser <enginlubem@ymail.com>
2024-01-02 23:18:42 +0100

    api documentation

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -11,6 +11,11 @@ use Transave\CommonBase\Actions\Auth\VerifyAccount;
 use Transave\CommonBase\Actions\Kuda\Account\CreateVirtualAccount;
 use Transave\CommonBase\Helpers\ResponseHelper;
 
+/**
+ * @group Authentication Controller Endpoints
+ *
+ * API routes for handling user authentication
+ */
 class AuthController extends Controller
 {
     use ResponseHelper;
@@ -33,6 +38,92 @@ class AuthController extends Controller
 
     /**
      * Login to existing account
+     *
+     * @bodyParam username string required The email or phone number of the user
+     * @bodyParam password string required The password of the user
+     * @response {
+     * "success": true,
+     * "data": {
+     * "id": "c0329e2b-67d8-4c30-8bd9-51208c0b5975",
+     * "first_name": "Emmanuel",
+     * "last_name": "Maduka",
+     * "middle_name": null,
+     * "business_name": null,
+     * "email": "emmanuel@raadaa.com",
+     * "phone": "+2348026459222",
+     * "account_number": "2504199022",
+     * "withdrawal_limit": "50000.00000000",
+     * "role": "customer",
+     * "verification_token": null,
+     * "is_verified": "yes",
+     * "account_type": "ordinary",
+     * "account_status": "verified",
+     * "created_at": "2023-09-25T07:17:05.000000Z",
+     * "updated_at": "2023-10-25T18:35:25.000000Z",
+     * "deleted_at": null,
+     * "token": "54|1Rp2Ricd0WuYLOsPfHrxjh0dChOWIX7ZZV5Xi8Rv",
+     * "account": {
+     * "percentage_completion": 0,
+     * "missing_fields": [
+     * "image_url",
+     * "identity_card_url",
+     * "address_proof_url",
+     * "identity_type",
+     * "identity_card_number",
+     * "country_of_origin_id",
+     * "country_of_residence_id",
+     * "state_id",
+     * "lga_id",
+     * "city",
+     * "next_of_kin",
+     * "next_of_kin_contact",
+     * "mother_maiden_name",
+     * "residential_status",
+     * "employment_status",
+     * "employer",
+     * "job_title",
+     * "educational_qualification",
+     * "income_range"
+     * ],
+     * "kyc": {
+     * "id": "078140cb-3693-4d9b-902d-b58797a50d60",
+     * "user_id": "c0329e2b-67d8-4c30-8bd9-51208c0b5975",
+     * "image_url": null,
+     * "identity_card_url": null,
+     * "address_proof_url": null,
+     * "identity_type": null,
+     * "identity_card_number": null,
+     * "country_of_origin_id": null,
+     * "country_of_residence_id": null,
+     * "state_id": null,
+     * "lga_id": null,
+     * "city": null,
+     * "next_of_kin": null,
+     * "next_of_kin_contact": null,
+     * "mother_maiden_name": null,
+     * "residential_status": null,
+     * "employment_status": null,
+     * "employer": null,
+     * "job_title": null,
+     * "educational_qualification": null,
+     * "date_of_employment": null,
+     * "number_of_children": null,
+     * "income_range": null,
+     * "verification_status": "unverified",
+     * "is_loan_compliant": "no",
+     * "created_at": "2023-09-25T07:17:05.000000Z",
+     * "updated_at": "2023-09-25T07:17:05.000000Z"
+     * }
+     * },
+     * "url": "/customer",
+     * "wallet": {
+     * "ledgerBalance": 4220600,
+     * "availableBalance": 4220600,
+     * "withdrawableBalance": 4220600
+     * }
+     * },
+     * "message": "user logged in successfully."
+     * }
      *
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Response
--- src/Http/Controllers/KudaAccountController.php
+++ src/Http/Controllers/KudaAccountController.php
@@ -13,6 +13,11 @@ use Transave\CommonBase\Actions\Kuda\Transfer\NameEnquiry;
 use Transave\CommonBase\Actions\Kuda\Transfer\VirtualAccountFundTransfer;
 use Transave\CommonBase\Actions\Kuda\Webhook\WebhookService;
 
+/**
+ * @group Kuda Transfer Controller Endpoints
+ *
+ * API routes for handling bank transfers though Kuda third party API
+ */
 class KudaAccountController extends Controller
 {
     public function __construct()
@@ -21,6 +26,25 @@ class KudaAccountController extends Controller
         $this->middleware('admin')->only('mainAccountTransfer');
     }
 
+    /**
+     * Get a listing of Kuda Banks
+     *
+     * @response [
+     * {
+     * "bankCode": "999129",
+     * "bankName": "Kudimoney(Kudabank)"
+     * },
+     * {
+     * "bankCode": "999044",
+     * "bankName": "Access Bank"
+     * },
+     * {
+     * "bankCode": "999057",
+     * "bankName": "Zenith Bank"
+     * }
+     * ]
+     * @return mixed
+     */
     public function bankList()
     {
         $response = (new BankList())->execute();
@@ -29,6 +53,34 @@ class KudaAccountController extends Controller
         return $response['data']['banks'];
     }
 
+    /**
+     * Query the details of a beneficiary
+     *
+     * @bodyParam beneficiary_account_number string required The beneficiary account number. for test, use 2504193714
+     * @bodyParam beneficiary_bank_code string required The beneficiary bank code obtained from kuda bank list. for test, use 999129
+     * @response {
+     * "data": {
+     * "beneficiaryAccountNumber": "2504193714",
+     * "beneficiaryName": "(Slait)-Stephen Francis",
+     * "senderAccountNumber": null,
+     * "senderName": null,
+     * "beneficiaryCustomerID": 0,
+     * "beneficiaryBankCode": "999129",
+     * "nameEnquiryID": 0,
+     * "responseCode": null,
+     * "transferCharge": 0,
+     * "sessionID": "NA"
+     * },
+     * "success": true,
+     * "message": "Request successful.",
+     * "meta_data": {
+     * "requestRef": "transave-202401021835-k5d815ujr",
+     * "serviceType": "NAME_ENQUIRY"
+     * }
+     * }
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse|NameEnquiry
+     */
     public function nameEnquiry(Request $request)
     {
         $response = (new NameEnquiry([
@@ -40,6 +92,29 @@ class KudaAccountController extends Controller
         return $response;
     }
 
+    /**
+     * Customer Account Fund Transfer
+     *
+     * @bodyParam user_id string required The primary key user ID of the sender. for test, use c0329e2b-67d8-4c30-8bd9-51208c0b5975
+     * @bodyParam beneficiary_account_number string required The account number of the beneficiary. for test, use 2504193714
+     * @bodyParam beneficiary_bank_code string required The bank code obtained from the kuda bank list for test, use 999129
+     * @bodyParam beneficiary_name string required The name of the beneficiary. for test, use 'Stephen Francis'
+     * @bodyParam amount integer required The amount to transfer in Naira
+     * @bodyParam narration string required A description of the transaction
+     * @bodyParam name_enquiry_sessionID string required The session ID obtained in beneficiary details endpoint. for test use 'NA'
+     * @bodyParam client_fee_charge intThe optional amount to charge for the transaction
+     * @response {
+     * "data": null,
+     * "success": true,
+     * "message": "Transaction successful",
+     * "meta_data": {
+     * "requestRef": "transave-202401021906-dax2ugaus",
+     * "serviceType": "VIRTUAL_ACCOUNT_FUND_TRANSFER"
+     * }
+     * }
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse|VirtualAccountFundTransfer
+     */
     public function virtualAccountTransfer(Request $request)
     {
         $response = (new VirtualAccountFundTransfer($request->all()))->execute();
@@ -47,12 +122,52 @@ class KudaAccountController extends Controller
         return $response;
     }
 
+    /**
+     * Main Account Fund Transfer
+     *
+     * @bodyParam beneficiary_account_number string required The account number of the beneficiary. for test, use 2504199022
+     * @bodyParam beneficiary_bank_code string required The bank code obtained from the kuda bank list for test, use 999129
+     * @bodyParam beneficiary_name string required The name of the beneficiary. for test, use 'maduka Emmanuel'
+     * @bodyParam amount int required The amount to transfer in Naira
+     * @bodyParam narration string required A description of the transaction
+     * @bodyParam name_enquiry_sessionID string required The session ID obtained in beneficiary details endpoint. for test use 'NA'
+     * @bodyParam client_fee_charge int The optional amount to charge for the transaction
+     * @response {
+     * "data": null,
+     * "success": true,
+     * "message": "Transaction successful",
+     * "meta_data": {
+     * "requestRef": "transave-202401021948-huyxmzovo",
+     * "serviceType": "SINGLE_FUND_TRANSFER"
+     * }
+     * }
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse|MainAccountFundTransfer
+     */
     public function mainAccountTransfer(Request $request)
     {
         $response = (new MainAccountFundTransfer($request->all()))->execute();
         return $response;
     }
 
+    /**
+     * Intra Bank Wallet to Wallet Transfer
+     *
+     * @bodyParam beneficiary_user_id string required The primary key User ID. For test, use e5951615-3f2c-4903-962f-d2cfdabd10cb
+     * @bodyParam amount int required The amount in naira to send
+     * @bodyParam narration string required The description of the transaction
+     * @response {
+     * "data": null,
+     * "success": true,
+     * "message": "Transaction successful",
+     * "meta_data": {
+     * "requestRef": "transave-202401022305-7kcsemxl0",
+     * "serviceType": "VIRTUAL_ACCOUNT_FUND_TRANSFER"
+     * }
+     * }
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse|IntrabankFundTransfer
+     */
     public function walletTransfer(Request $request)
     {
         $response = (new IntrabankFundTransfer([
