3948064b4b7eaf12e7962806d3c8109a10c9fa85
Emmanuel Maduka <meckyblaze@yahoo.com>
2025-04-03 15:59:53 +0100

    re(ResetPassword): handle null token

--- src/Actions/Auth/ResetPassword.php
+++ src/Actions/Auth/ResetPassword.php
@@ -38,6 +38,7 @@ class ResetPassword extends Action
     private function setUser()
     {
         $reset = DB::table('password_resets')->where("token", $this->validatedInput['token'])->first();
+        abort_if(!$reset, 403, "invalid or expired token");
         $this->user = User::query()->where("phone", $reset->email)->first();
 
         $isExpired = Carbon::now()->gt(Carbon::parse($reset->created_at)->addMinutes(10));
